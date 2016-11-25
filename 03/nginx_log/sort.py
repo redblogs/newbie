ddf499e0d72489db708bdd146d61ef34d58981f5
Fuliwei <root@xiaowei.python.com>
2016-11-25 15:14:18 +0800

    2016-11-25

--- 03/nginx_log/sort.py
+++ 03/nginx_log/sort.py
@@ -1,16 +1,27 @@
 d = {'10.35.1.82': 61, '101.227.4.33': 2, '111.85.41.230': 103,'218.200.66.196': 14,'218.200.66.197': 21,'218.200.66.198': 25,'218.200.66.199': 19,'220.172.215.69': 1,'220.181.125.177': 1,'221.176.5.153': 31,'221.176.5.154': 3,'221.176.5.156': 3}
 
 tmp = d.items()
-print tmp
+#print tmp
+#le = len(tmp)
+#for i in range(le-1):
+#	for j in range(le-1-i):
+#		if tmp[j][1] < tmp[j+1][1]:
+#			tmp[j],tmp[j+1] = tmp[j+1],tmp[j]
+
+
+#print "sorted tmp :%s" %tmp
+
+def bubble_sort(tmp,le,num=3):
+	for i in range(le-1):
+		for j in range(le-1-i):
+			if tmp[j][1] < tmp[j+1][1]:
+				tmp[j],tmp[j+1] = tmp[j+1],tmp[j]
+	tmp = tmp[:num]
+	return tmp
+
+
+
 le = len(tmp)
-for i in range(le-1):
-	for j in range(le-1-i):
-		if tmp[j][1] < tmp[j+1][1]:
-			tmp[j],tmp[j+1] = tmp[j+1],tmp[j]
-
-
-print "sorted tmp :%s" %tmp
-
-tmp = tmp[:3]
-for i in tmp:
+res = bubble_sort(tmp,le,num=4)
+for i in res:
 	print "ip is %s ,count is %s" %i 
--- 03/writef.py
+++ 03/writef.py
@@ -1,4 +1,6 @@
+#coding:utf-8
 res = {'!': 1, '.': 1, 'Bob': 1, 'I': 2, 'a': 1, 'am': 2, 'boy': 1}
+
 
 new = {}
 for k ,v in res.items():
@@ -8,20 +10,20 @@ for k ,v in res.items():
 print new
 #keys = max(new)
 #print "%s  ----> %s" %(keys,new[keys])
+def op_file(filename,mode='r'):
+	f = open(filename,mode)
+	str = "/usr/local/src/newbie/03==>result\n<table border='1px'><tr><th>times</th><th>word</th></tr>"
+	while True:
+		keys = max(new)
+		for i in new[keys]:
+			str += "<tr><td>%s</td><td>%s</td></tr>" %(keys,i)
+		new.pop(keys)
+		if len(new) == 0 :
+			break
 
-f = open('tongji.html',"w+")
-str = "<table border='1px'><tr><th>times</th><th>word</th></tr>"
-while True:
-	keys = max(new)
-	for i in new[keys]:
-		str += "<tr><td>%s</td><td>%s</td></tr>" %(keys,i)
-	new.pop(keys)
-	if len(new) == 0 :
-		break
+	str += "</table>\n"
+	if ('w' in mode) or ('a' in mode) :		##使用in来判断模式是否为写模式
+		f.write(str)
 
-str += "</table>\n"
-
-f.write(str)
-
-f.close()
-
+	f.close()
+op_file(filename='/var/www/html/tongji.html',mode="w+")
