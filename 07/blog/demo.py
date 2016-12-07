d5cef2520363954e81c1727002dccf739e42e5e5
Fuliwei <769392488@qq.com>
2016-12-07 17:40:08 +0800

    重新整理复习更新删除思路

--- 07/blog/demo.py
+++ 07/blog/demo.py
@@ -9,6 +9,9 @@ datas = mysql.connect(user='root',passwd='123456',db='reboot',charset='utf8')
 datas.autocommit(True)
 cur = datas.cursor()
 
+
+fields = ["id","name","name_cn","password","email","mobile","role","status","create_time"]
+
 app = Flask(__name__)
 
 def getNow():
@@ -17,7 +20,6 @@ def getNow():
 
 @app.route('/user/userlist')
 def userlist():
-	fields = ["id","name","name_cn","password","email","mobile","role","status","create_time"]
 	sql = "select %s from %s" %(','.join(fields),'users')	
 	cur.execute(sql)
 	res = cur.fetchall()
@@ -31,9 +33,37 @@ def delete():
 	d_sql = "No id error"
 	if id :
 		d_sql = "DELETE from users where id = %s" %  id
-		#cur.execute(d_sql)
+		cur.execute(d_sql)
 		return redirect('/user/userlist')
 	return render_template('test.html',errmes = d_sql)
+
+@app.route('/user/update',methods=['GET','POST'])
+def update():
+	if request.method == "POST":
+		user = dict(request.form)
+		user = dict((k,v[0]) for k ,v in user.items())
+		#print user ==>{'mobile': u'CCC', 'name_cn': u'ccccccc', 'id': u'2', 'name': u'cc', 'email': u'CC@chinacache.com'}
+		#user.pop('id')
+		tmp_list = [ "%s='%s'"%(k,v) for k,v  in user.items() if k != "id"]
+		#print tmp_list
+		update_sql = "UPDATE users set %s  where id=%s" %(','.join(tmp_list),user['id'])
+		cur.execute(update_sql)
+		return redirect('/user/userlist')
+	else:
+		id = request.args.get('id',None)
+		print id
+		select_sql = "No id error"
+		if id :
+			select_sql = "select %s from users  where id = %s" %(','.join(fields),id)
+			print select_sql
+			cur.execute(select_sql)
+			res = cur.fetchone()
+			#print res ==>(2L, u'cc', u'cc', u'cC23 ', u'CC@chinacache.com', u'CCC', u'user', 0, datetime.datetime(2016, 11, 30, 16, 4, 33))
+			user = dict((v,res[k]) for k ,v in enumerate(fields))
+			
+			#return redirect('/user/userlist')
+		return render_template('userinfo.html',user = user)
+
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0',port=888,debug=True)
